1f89d23c3a052bebb5eee0fb8d76643f82d356e8
Eitan Suez <eitan.suez@solo.io>
2025-11-07 08:15:20 -0600

    switching to local registry

--- k8s-deploy-skill/build-image.sh
+++ k8s-deploy-skill/build-image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build --push -t eitansuez/k8s-simple-deploy:latest .
+# docker build --push -t eitansuez/k8s-simple-deploy:latest .
+docker build --push -t localhost:5010/k8s-simple-deploy:latest .
